b8a41eae2b449ee75f6f32822580a16d308c5ab2
kiminjoong <ijboym216@gmail.com>
2022-11-20 23:22:46 +0900

    1. 랜딩 작업중

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,37 +16,116 @@ const Home = ({ countries }: any) => {
     <div style={{ display: 'flex', flexDirection: 'column' }}>
 
       <Header />
-      <div style={{ display: 'flex', flexDirection: 'column', flex: 1, overflow: 'scroll' }}>
-        <div style={{ backgroundColor: '#F8F8F8', display: 'flex', flexDirection: 'row', height: 699 }}>
-          <div style={{ flex: 1, border: '1px solid black' }}></div>
-          <div style={{ paddingTop: 160, border: '1px solid black' }}>
-            <div>
-              <p style={{ fontSize: 50, fontWeight: 600 }}>문화를 통해 공간을</p>
-              <p style={{ fontSize: 50, fontWeight: 600 }}>재생하다.</p>
+      <div style={{ display: 'flex', flexDirection: 'column', overflow: 'scroll', paddingTop: 60, backgroundColor: 'white', }}>
+        <section style={{ backgroundColor: '#F8F8F8', height: 699, }}>
+          <div style={{ maxWidth: '1100px', margin: 'auto', position: 'relative' }}>
+            <div style={{ paddingTop: 160, }}>
+              <div>
+                <p style={{ fontSize: 50, fontWeight: 600 }}>문화를 통해 공간을</p>
+                <p style={{ fontSize: 50, fontWeight: 600 }}>재생하다.</p>
+              </div>
+              <div style={{ marginTop: 28 }}>
+                <p>지역 역사와 문화의 고유한 가치를 콘텐츠로 개발하여</p>
+                <p>지역사회의 가치를 상승시키고 시민들의 삶의 질을 높이고자 합니다.</p>
+
+              </div>
             </div>
-            {/* <div style={{ marginTop: 28 }}>
-              <p style={{ fontSize: 18, fontWeight: 300 }}>지역 역사와 문화의 고유한 가치를 콘텐츠로 개발하여</p>
-              <p style={{ fontSize: 18, fontWeight: 300 }}>지역사회의 가치를 상승시키고 시민들의 삶의 질을 높이고자 합니다.</p>
-            </div> */}
-            <div style={{ marginTop: 66 }}>
-              <Image src={require('../assets/images/main.png')} alt={'main'} width={900} height={438} />
+
+            <div style={{ marginTop: 66, }}>
+              <Image src={require('../assets/images/main.png')} alt={'main'} style={{ width: '1100px', height: '438px' }} />
             </div>
-          </div>
-          <div style={{ flex: 1, border: '1px solid black', position: 'relative' }}>
-            <div style={{ position: 'absolute', top: -55, left: -40 }}>
+            <div style={{ position: 'absolute', top: -60, right: 0, marginRight: '104px' }}>
               <Image className="img" src={require('../assets/images/main_text.png')} alt={'main_text'} width={254} height={259} />
             </div>
-          </div>
-          <div style={{ flex: 1, border: '1px solid black', position: 'relative' }}>
-            <div style={{ position: 'absolute', top: 152, right: -93, padding: '13px 121px 14px 36px', backgroundColor: color.brand, transform: 'rotate(90deg)' }}>
-              <span>NABILERA</span>
+
+            <div style={{ position: 'absolute', top: 0, right: 0 }}>
+
+              <div style={{ padding: '36px 13px 121px 14px', backgroundColor: '#F6C42D' }}>
+                <span style={{ textOrientation: 'sideways', writingMode: 'vertical-lr', letterSpacing: 15 }}>NABILERA</span>
+              </div>
             </div>
           </div>
-          <div style={{ flex: 1, border: '1px solid black' }}>
+
+        </section>
+        <section style={{ height: 679 }}>
+          <div style={{ maxWidth: '1100px', margin: 'auto', display: 'flex', flexDirection: 'row', position: 'relative', }}>
+            <div style={{ marginTop: 288 }}>
+              <Image src={require('../assets/images/main_news.png')} alt={'main'} style={{ width: '367px', height: '477px' }} />
+            </div>
+            <div style={{ marginLeft: 92, marginTop: 304, flex: 1 }}>
+              <h2 style={{ fontSize: 50 }}>News</h2>
+              <ul style={{ marginTop: 31 }}>
+                <li style={{ display: 'flex', flexDirection: 'row', paddingBottom: 15, borderBottomWidth: 1, borderBottomColor: 'black', borderBottomStyle: 'solid' }}>
+                  <p style={{ flex: 1, fontSize: 15 }}>[춘천사람들] [취재기자 현장 인터뷰] (사)강원살이 청년이 행복한 춘천을 꿈꾼다</p>
+                  <p style={{ fontSize: 14 }}>2022.11.10</p>
+                </li>
+              </ul>
+            </div>
           </div>
 
-        </div>
+        </section>
+        <section style={{ backgroundColor: color.N40, }}>
+          <div style={{ maxWidth: 1100, margin: 'auto', position: 'relative', height: 1152 }}>
+            <div style={{ position: 'absolute', top: 132, right: 92, backgroundColor: '#FDFDFD', padding: '62px 0px 60px 33px' }}>
+              <div style={{ padding: '62px 103px 0px 0px' }}>
+                <Image src={require('../assets/images/mission.png')} alt={'main'} style={{ width: 131, height: 91 }} />
+              </div>
+              <div style={{ marginTop: 61 }}>
+                <h3>미션</h3>
+                <p style={{ marginTop: 20, maxWidth: 211 }}>
+                  지역의 문화자원을 창의적 사고와 끝없는 도전을 통해 콘텐츠를 개발함으로써 지역의 문화를 바꾼다.
+                </p>
+              </div>
+            </div>
 
+
+            <div style={{ position: 'absolute', top: 313, left: 365.5, backgroundColor: '#FDFDFD', padding: '62px 0px 60px 33px' }}>
+              <div style={{ padding: '62px 103px 0px 0px' }}>
+                <Image src={require('../assets/images/vision.png')} alt={'main'} style={{ width: 131, height: 91 }} />
+              </div>
+              <div style={{ marginTop: 61 }}>
+                <h3>비전</h3>
+                <p style={{ marginTop: 20, maxWidth: 211 }}>
+                  지역 역사와 문화를 통해 공간을 재생하여 국내 및 해외 관광객들에게 지역 문화 콘텐츠 제공
+                </p>
+              </div>
+            </div>
+
+
+
+            <div style={{ position: 'absolute', bottom: 146, right: 92, backgroundColor: '#FDFDFD', padding: '62px 0px 60px 33px' }}>
+              <div style={{ padding: '62px 103px 0px 0px' }}>
+                <Image src={require('../assets/images/value.png')} alt={'main'} style={{ width: 131, height: 91 }} />
+              </div>
+              <div style={{ marginTop: 61 }}>
+                <h3>가치</h3>
+                <p style={{ marginTop: 20, maxWidth: 211 }}>
+                  #지역 #문화 #재생
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section style={{ backgroundColor: 'white', paddingTop: 106, paddingBottom: 40 }}>
+          <div style={{ maxWidth: 1100, margin: 'auto', }}>
+            <div>
+              <h2 style={{ fontSize: 50 }}>History</h2>
+              <div style={{ display: 'flex', flexDirection: 'row', marginTop: 103, borderTop: '1px solid black', padding: '30px 0px 30px 0px' }}>
+                <div>
+                  <span style={{ fontSize: 35 }}>2022</span>
+                </div>
+                <div style={{ marginLeft: 182, flex: 1 }}>
+                  <ul style={{ listStyleType: 'circle' }}>
+                    <li>
+                      <span>dasd</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </section>
       </div>
       <Footer />
     </div >
